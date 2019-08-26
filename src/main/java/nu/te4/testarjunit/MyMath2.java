736a903eeeee8d2218553b17cb9e8c835d73b1dd
dlufr06 <dlufr06@gmail.com>
2019-08-26 21:44:35 +0200

    Initial commit

---
+++ src/main/java/nu/te4/testarjunit/MyMath2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nu.te4.testarjunit;
+
+/**
+ *
+ * @author Daniel
+ */
+public class MyMath2 {
+       public double convertToFarenheit(double c){           
+         double f = 1.8*c + 32;
+        return f; 
+    }
+    
+    public int compareTo(int a, int b){
+        
+        //om a > b --> 1
+        
+        //om a == b --> 0
+        
+        //om a < b --> -1
+        return 0; 
+    }
+}
