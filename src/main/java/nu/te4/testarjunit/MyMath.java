a63368e758021131b94670a8701c0c1e1f04a667
dlufr06 <dlufr06@gmail.com>
2019-08-27 10:46:37 +0200

    added methods

--- src/main/java/nu/te4/testarjunit/MyMath.java
+++ src/main/java/nu/te4/testarjunit/MyMath.java
@@ -10,5 +10,42 @@ package nu.te4.testarjunit;
  * @author Daniel
  */
 public class MyMath {
+        public int add(String a, String b){       
+        int tal1, tal2; 
+        try {
+            tal1 = Integer.parseInt(a);
+            tal2 = Integer.parseInt(b);
+            return tal1+tal2;
+        } catch (Exception e) { //Vilket exceptions är det vi får här? kan förbättras.
+            throw new NumberFormatException("Du måste ha siffror");
+        }
+    }
     
+    public boolean isEqual(int a, int b){
+        return a == b;
+    }
+    
+    public int difference(int a, int b){
+        //skillnaden mellan a och b; 
+        
+        //om a = 4 och b = 2 -> 2
+        
+        //om a = 2 och b = 4 -> 2
+        
+        return 0;
+    }
+    
+    public int difference(String a,String b){
+        //skillnaden mellan a och b; 
+        
+        //om a = 4 och b = 2 -> 2
+        
+        //om a = 2 och b = 4 -> 2
+        
+        //talen skall även parsas 
+        
+        //Fel förväntas om man lägger in fel.
+        
+        return 0;
+    }
 }
